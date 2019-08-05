9bf9fd1869317eb0b181cfbbda4f4ebfa458e0a5
Jennifer Silva <jennifersilva20@gmail.com>
2019-08-05 06:42:55 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,17 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(int)
+  if int % 3 == 0
+    return "Fizz"
+  end
+  elif int % 5 == 0
+    retun "Buzz"
+  end
+  elif int % 3 == 0 && int % 5 == 0
+    return "FizzBuzz"
+  end
+  else 
+    puts "nil"
+  end
+end 
